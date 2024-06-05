8852576c3bd372e3c9f0884cf99b7109bebd6565
wyz <wangyuzhuo@arksoft.ltd>
2024-06-05 15:49:58 +0800

    添加github打包配置

--- emlibrary/build.gradle.kts
+++ emlibrary/build.gradle.kts
@@ -38,3 +38,16 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
 }
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.github.Harbor2"
+                artifactId = "emlibrary"
+                version = "1.0.6"
+            }
+        }
+    }
+}
