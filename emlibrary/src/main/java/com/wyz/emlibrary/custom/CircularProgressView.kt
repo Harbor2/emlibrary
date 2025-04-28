719b2a6e0eae4e1a27cbad282cfdbc443e8c438f
wyz <wangyuzhuo@arksoft.ltd>
2025-04-28 13:32:12 +0800

    CircularProgressView 支持渐变色

--- emlibrary/src/main/java/com/wyz/emlibrary/custom/CircularProgressView.kt
+++ emlibrary/src/main/java/com/wyz/emlibrary/custom/CircularProgressView.kt
@@ -2,13 +2,14 @@ package com.wyz.emlibrary.custom
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Matrix
 import android.graphics.Paint
+import android.graphics.SweepGradient
 import android.util.AttributeSet
 import android.view.View
 import androidx.annotation.ColorInt
 import androidx.core.content.ContextCompat
 import com.wyz.emlibrary.R
-import com.wyz.emlibrary.util.EMUtil
 import kotlin.math.max
 import kotlin.math.min
 
@@ -26,6 +27,11 @@ class CircularProgressView @JvmOverloads constructor(
     private val trackPaint: Paint
     private val progressPaint: Paint
     private var progress = 0
+
+    private var progressGradient: SweepGradient? = null
+    private var gradientColors: IntArray? = null
+    private var gradientPosition: FloatArray? = null
+
     init {
         val typedArray = context.theme.obtainStyledAttributes(
             attrs,
@@ -33,8 +39,8 @@ class CircularProgressView @JvmOverloads constructor(
             0, 0
         )
 
-        val trackColor = typedArray.getColor(R.styleable.CircularProgressView_cpbTrackColor, ContextCompat.getColor(context, android.R.color.darker_gray))
-        val progressColor = typedArray.getColor(R.styleable.CircularProgressView_cpbProgressColor, ContextCompat.getColor(context, android.R.color.holo_blue_light))
+        val trackColor = typedArray.getColor(R.styleable.CircularProgressView_cpbTrackColor, ContextCompat.getColor(context, android.R.color.transparent))
+        val progressColor = typedArray.getColor(R.styleable.CircularProgressView_cpbProgressColor, ContextCompat.getColor(context, android.R.color.transparent))
         val trackThickness = typedArray.getDimension(R.styleable.CircularProgressView_cpbTrackThickness, 4f)
         val progressThickness = typedArray.getDimension(R.styleable.CircularProgressView_cpbProgressThickness, 6f)
         val isRoundIndication = typedArray.getBoolean(R.styleable.CircularProgressView_cpbRoundIndication, true)
@@ -62,19 +68,41 @@ class CircularProgressView @JvmOverloads constructor(
         super.onDraw(canvas)
         val width = width.toFloat()
         val height = height.toFloat()
+        val centerX = width / 2
+        val centerY = height / 2
         val radii = min(width, height) / 2 - max(trackPaint.strokeWidth, progressPaint.strokeWidth) / 2
 
         // Draw track
-        canvas.drawCircle(width / 2, height / 2, radii, trackPaint)
+        canvas.drawCircle(centerX, centerY, radii, trackPaint)
+
+        // Update shader if needed
+        gradientColors?.let { colors ->
+            if (progressGradient == null) {
+                // position 校验
+                gradientPosition?.let {
+                    if (it.size != colors.size) {
+                        gradientPosition = null
+                    }
+                }
+                progressGradient = SweepGradient(centerX, centerY, colors, gradientPosition)
+
+                // 这里加一个旋转，使渐变起点到12点方向
+                val matrix = Matrix()
+                matrix.postRotate(-90f, centerX, centerY) // 逆时针旋转90度
+                progressGradient?.setLocalMatrix(matrix)
+
+                progressPaint.shader = progressGradient
+            }
+        }
 
         // Draw progress
         val sweepAngle = 360 * (progress / 100f)
         canvas.drawArc(
-            width / 2 - radii,
-            height / 2 - radii,
-            width / 2 + radii,
-            height / 2 + radii,
-            -90f,
+            centerX - radii,
+            centerY - radii,
+            centerX + radii,
+            centerY + radii,
+            -90f,  // 12点方向起始
             sweepAngle,
             false,
             progressPaint
@@ -98,6 +126,13 @@ class CircularProgressView @JvmOverloads constructor(
 
     fun setProgress(value: Int) {
         progress = value
+        invalidate()
+    }
+
+    fun setIndicationGradient(@ColorInt colors: IntArray, position: FloatArray? = null) {
+        gradientColors = colors
+        progressGradient = null
+        gradientPosition = position
         invalidate()
     }
 }
