84fdd8e99de658f801e0ed01dbfb1ec09dfc2c82
wyz <wangyuzhuo@arksoft.ltd>
2025-11-11 20:48:41 +0800

    DialogChain链式调用

--- emlibrary/src/main/java/com/wyz/emlibrary/util/dialog/DialogChain.kt
+++ emlibrary/src/main/java/com/wyz/emlibrary/util/dialog/DialogChain.kt
@@ -4,7 +4,69 @@ import android.util.Log
 import com.wyz.emlibrary.TAG
 
 /**
- * DialogChain.Builder().addIntercept().build().proceed()
+ * DialogChain.Builder()
+ *                 .addIntercept(TestDialog1(this).apply {
+ *                     setData()
+ *                     mCallback = {}
+ *                 })
+ *                 .addIntercept(TestDialog2(this).apply {
+ *                     setData()
+ *                     mCallback = {}
+ *                 })
+ *                 .addIntercept(TestDialog1(this).apply {
+ *                     setData()
+ *                     mCallback = {}
+ *                 })
+ *                 .build()
+ *                 .proceed()
+ *
+ *
+ * class TestDialog1(private val activity: Activity) : Dialog(activity), DialogIntercept {
+ *     var binding: LayoutDialog1Binding
+ *     private var dialogChain: DialogChain? = null
+ *     var mCallback: (() -> Unit)? = null
+ *
+ *     init {
+ *         window?.setBackgroundDrawableResource(R.color.transparent)
+ *         binding = LayoutDialog1Binding.inflate(LayoutInflater.from(context))
+ *         setContentView(binding.root)
+ *         setCancelable(true)
+ *         setCanceledOnTouchOutside(true)
+ *         initListener()
+ *     }
+ *
+ *     fun setData() {
+ *         // 设置对话框数据
+ *     }
+ *
+ *     private fun initListener() {
+ *         binding.tvClose.setOnClickListener {
+ *             dismiss()
+ *         }
+ *         // 监听对话框关闭事件，确保链式调用继续
+ *         setOnDismissListener {
+ *             dialogChain?.proceed()
+ *         }
+ *     }
+ *
+ *     override fun intercept(dialogIntercept: DialogChain) {
+ *         this.dialogChain = dialogIntercept
+ *         if (activity.isFinishing || activity.isDestroyed) {
+ *             dialogIntercept.proceed()
+ *             return
+ *         }
+ *         val window: Window? = this.window
+ *         window?.let {
+ *             it.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+ *             it.setGravity(Gravity.CENTER)
+ *         }
+ *         show()
+ *     }
+ *
+ *     override fun needShow(): Boolean {
+ *         return true
+ *     }
+ * }
  */
 class DialogChain(private val builder: Builder) {
     private var index = 0
