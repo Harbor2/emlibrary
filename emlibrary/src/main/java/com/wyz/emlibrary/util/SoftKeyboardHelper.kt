be2e261e4fc81dee13e3ee3ac93fd3736cc5e70a
wyz <wangyuzhuo@arksoft.ltd>
2026-05-08 19:50:33 +0800

    SoftKeyboardHelper.kt

--- emlibrary/src/main/java/com/wyz/emlibrary/util/SoftKeyboardHelper.kt
+++ emlibrary/src/main/java/com/wyz/emlibrary/util/SoftKeyboardHelper.kt
@@ -1,9 +1,13 @@
 package com.wyz.emlibrary.util
 
 import android.app.Activity
+import android.content.Context
 import android.graphics.Rect
+import android.os.IBinder
 import android.util.Log
 import android.view.ViewTreeObserver.OnGlobalLayoutListener
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import com.wyz.emlibrary.TAG
 
 /**
@@ -94,6 +98,29 @@ class SoftKeyboardHelper {
             viewTreeObserver.removeOnGlobalLayoutListener(onGlobalLayoutListener)
         }
     }
+
+    fun showSoftKeyboard(view: EditText, context: Context, delay: Long = 200) {
+        view.postDelayed({
+            view.requestFocus()
+            val inputMethodManager =
+                context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.showSoftInput(view, 0)
+        }, delay)
+    }
+
+    fun hideSoftKeyboard(view: EditText, context: Context) {
+        view.clearFocus()
+        val inputMethodManager =
+            context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun hideSoftKeyboard(windowToken: IBinder, context: Context) {
+        val inputMethodManager =
+            context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
+    }
+
 
     interface OnSoftKeyBoardChangeListener {
         fun keyBoardShow(height: Int)
