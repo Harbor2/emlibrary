78e7dfedbe07678bb14d9849a8e5b165f6d1d6fd
wyz <wangyuzhuo@arksoft.ltd>
2026-05-25 20:51:16 +0800

    TestActivity.kt

--- app/src/main/java/com/wyz/test/ui/TestActivity.kt
+++ app/src/main/java/com/wyz/test/ui/TestActivity.kt
@@ -3,9 +3,21 @@ package com.wyz.test.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.wyz.app.databinding.ActivityTestBinding
+import com.wyz.emlibrary.TAG
 import com.wyz.emlibrary.util.immersiveWindowC
+import com.wyz.test.ui.viewmodel.TestViewModel
+import com.wyz.test.ui.viewmodel.TestViewModelFactory
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 class TestActivity : AppCompatActivity() {
@@ -17,6 +29,10 @@ class TestActivity : AppCompatActivity() {
     }
 
     private lateinit var binding: ActivityTestBinding
+    private val viewModel: TestViewModel by viewModels {
+        TestViewModelFactory()
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +40,41 @@ class TestActivity : AppCompatActivity() {
         immersiveWindowC(binding.root, true)
 
         setContentView(binding.root)
+
+        update()
+        setupObserver()
+
+        lifecycleScope.launch {
+            delay(1000)
+            viewModel.setSharedState("11111")
+        }
     }
 
+    private fun update() {
+        lifecycleScope.launch {
+            delay(5000)
+            viewModel.setSharedState("22222")
+            viewModel.setState("hhhhhh")
+
+            Log.d(TAG, "后台数据发生变化")
+        }
+    }
+
+    private fun setupObserver() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.stateFlow.collect { value ->
+                    Log.d(TAG, "STATE 接收到数据：$value")
+
+                }
+            }
+        }
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.sharedFlow.collect { info ->
+                    Log.d(TAG, "SHARED 接收到数据：$info")
+                }
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/wyz/test/ui/viewmodel/TestViewModel.kt
@@ -0,0 +1,45 @@
+package com.wyz.test.ui.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.wyz.emlibrary.TAG
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+
+class TestViewModel() : ViewModel() {
+    private val _loadObserver = MutableLiveData(false)
+    val loadObserver = _loadObserver
+    private fun load() {
+        _loadObserver.value = true
+    }
+
+
+    private val _stateFlow = MutableStateFlow("default")
+    val stateFlow = _stateFlow
+    fun setState(state: String) {
+        _stateFlow.value = state
+    }
+
+
+    private val _sharedFlow = MutableSharedFlow<String>(replay = 0)
+    val sharedFlow = _sharedFlow.asSharedFlow()
+    fun setSharedState(state: String) {
+        viewModelScope.launch {
+            Log.d(TAG, "sharedFLow 发送消息：")
+            _sharedFlow.emit(state)
+        }
+    }
+
+}
+
+class TestViewModelFactory() : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return TestViewModel() as T
+    }
+}
